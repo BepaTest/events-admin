e41209efdcbf4dff81e95d40a0b2c5b87acd12c5
Vera Testard <vera.testard@gmail.com>
2020-10-15 11:34:01 +0200

    Updates

---
+++ precache-manifest.37322584d4596af8c97b0492dca4e95c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8967e1a059ce8feaeb24",
+    "url": "/BepaTest/events-admin/static/js/runtime~main.8967e1a0.js"
+  },
+  {
+    "revision": "a8ae49d2a35b0cd3925a",
+    "url": "/BepaTest/events-admin/static/js/main.a8ae49d2.chunk.js"
+  },
+  {
+    "revision": "e7f31dcd002cbf437f3a",
+    "url": "/BepaTest/events-admin/static/js/1.e7f31dcd.chunk.js"
+  },
+  {
+    "revision": "92fae8ebaf2cd0b08200a8e2f80bada6",
+    "url": "/BepaTest/events-admin/index.html"
+  }
+];
