0c28193d5b17c61327f13e731436963b1e8879ff
Vera Testard <vera.testard@gmail.com>
2020-11-02 15:09:50 +0100

    chavanay adapted version

---
+++ src/FIREBASE_CONFIG.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAQM6ktPaKMmwMsheXguHdTURKg4nzwWeU",
+  authDomain: "communeevents-581a9.firebaseapp.com",
+  databaseURL: "https://communeevents-581a9.firebaseio.com",
+  projectId: "communeevents-581a9",
+  storageBucket: "communeevents-581a9.appspot.com",
+  messagingSenderId: "679013840468",
+  appId: "1:679013840468:web:07896271dff0db0fbe1fc1",
+  measurementId: "G-Z2FF092H8V"
+};
