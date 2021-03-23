b31fa14a474b1a3d2665923f4dd704d7d528f278
macbook <macbook@vera.home>
2021-03-23 15:37:13 +0100

    feat: react-admin and all auther dependency versions updated

--- src/App.js
+++ src/App.js
@@ -1,52 +1,54 @@
-import * as React from "react";
-import { EventList, EventShow, EventCreate, EventEdit } from "./events";
-import { Admin, Resource } from "react-admin";
+import * as React from 'react'
+import { EventList, EventShow, EventCreate, EventEdit } from './events'
+import { Admin, Resource, Login } from 'react-admin'
 import {
   FirebaseRealTimeSaga,
   FirebaseDataProvider,
   FirebaseAuthProvider,
-} from "react-admin-firebase";
-import firebase from "firebase";
-import CustomLoginPage from "./CustomLoginPage";
-import frenchMessages from "ra-language-french";
-import { firebaseConfig } from "./FIREBASE_CONFIG";
+} from 'react-admin-firebase'
+import firebase from 'firebase'
+import frenchMessages from 'ra-language-french'
+import polyglotI18nProvider from 'ra-i18n-polyglot'
+import { firebaseConfig } from './FIREBASE_CONFIG'
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig)
 
 const options = {
   logging: true,
   app: firebaseApp,
   // rootRef: 'root_collection/some_document',
-};
-
-const dataProvider = FirebaseDataProvider(firebaseConfig, options);
-const authProvider = FirebaseAuthProvider(firebaseConfig, options);
-const firebaseRealtime = FirebaseRealTimeSaga(dataProvider, options);
-
-const i18nProvider = () => frenchMessages;
-
-class App extends React.Component {
-  render() {
-    return (
-      <Admin
-        loginPage={CustomLoginPage}
-        customSagas={[firebaseRealtime]}
-        dataProvider={dataProvider}
-        authProvider={authProvider}
-        locale="fr"
-        i18nProvider={i18nProvider}
-      >
-        <Resource name="categories" />
-        <Resource
-          name="evenements"
-          list={EventList}
-          show={EventShow}
-          create={EventCreate}
-          edit={EventEdit}
-        />
-      </Admin>
-    );
-  }
 }
 
-export default App;
+const dataProvider = FirebaseDataProvider(firebaseConfig, options)
+const authProvider = FirebaseAuthProvider(firebaseConfig, options)
+//const firebaseRealtime = FirebaseRealTimeSaga(dataProvider, options)
+
+const i18nProvider = polyglotI18nProvider(() => frenchMessages, 'fr')
+
+const MyLoginPage = () => (
+  <Login
+    // Condrieu Picture
+    backgroundImage='https://www.parlezmoidimmo.fr/public/files/images/ou-vivre-dans-le-rhone.jpg'
+  />
+)
+
+const App = () => (
+  <Admin
+    loginPage={MyLoginPage}
+    //customSagas={[firebaseRealtime]}
+    dataProvider={dataProvider}
+    authProvider={authProvider}
+    i18nProvider={i18nProvider}
+  >
+    <Resource name='categories' />
+    <Resource
+      name='evenements'
+      list={EventList}
+      show={EventShow}
+      create={EventCreate}
+      edit={EventEdit}
+    />
+  </Admin>
+)
+
+export default App
